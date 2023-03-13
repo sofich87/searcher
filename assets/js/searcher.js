4701ae79330679949281ea918c150f425575f883
Sofia Chiriboga <sofich87@Sofias-MacBook-Pro.local>
2023-03-13 16:22:04 -0500

    searcher 1

--- assets/js/searcher.js
+++ assets/js/searcher.js
@@ -1,8 +1,55 @@
+let URLjson = 'https://raw.githubusercontent.com/Bootcamp-Espol/Datos/main/products.json'
+let URLxml = 'https://raw.githubusercontent.com/Bootcamp-Espol/Datos/main/products.xml'
 
-let loadProducts=() =>{
-  let URLjson = 'https://raw.githubusercontent.com/Bootcamp-Espol/Datos/main/products.json'
-  let URLxml = 'https://raw.githubusercontent.com/Bootcamp-Espol/Datos/main/products.xml'
+let element = document.getElementById('filter');
+  fetch(URLjson)
+  .then(response => response.json() ) 
+  .then(result => {
+
+    element.onclick = (event) => {
+      
+      let text = document.getElementById('text').value;
+      let idtag= document.getElementById('input1');
+      idtag.innerHTML ='';
+      result.forEach(element => {
+        let src = element.src;
+        let name = element.name;
+        let type = element.type;
+        let price = element.price;
+        if(text.includes(name) || text.includes(type)){
+          
+          let plantilla = `<div class="col-xl-3 col-md-6 mb-xl-0 mb-4 mt-4">
+            <div class="card card-blog card-plain">
+            <div class="card-header p-0 mt-n4 mx-3">
+              <a class="d-block shadow-xl border-radius-xl">
+              <img src="${src}" alt="${name}" class="img-fluid shadow border-radius-xl">
+            </a>
+            </div>
+            <div class="card-body p-3">
+              <p class="mb-0 text-sm">${type}</p>
+              <a href="javascript:;">
+                <h5>
+                  ${name}
+                </h5>
+              </a>
+              <p class="mb-4 text-sm">
+                <b>Price: </b> $ ${price}
+              </p>
+              </div>
+            </div>
+            </div>`
+          idtag.innerHTML+=plantilla;
   
+        }
+          
+      });
+      }
+
+    });
+
+
+let loadProducts=(URLjson, URLxml) =>{
+    
   fetch(URLjson)
       .then(response => response.json() ) 
       .then(result => {
@@ -80,36 +127,49 @@ let loadProducts=() =>{
     }
     
   })
+  
   .catch(error => {
     /* Callback por fallo: Procese el error */
     console.log( error );
   })
-
-  
 };
 
-loadProducts();
-
-
-/*let element = document.getElementById("dropdownMenuButton");
-
-    element.addEventListener('change', (event) => {
+loadProducts(URLjson,URLxml);
+      
+     // let xml = (new DOMParser()).parseFromString(result, 'application/xml');
+      
     //Código a ejecutar
-    //El event contiene la información del elemento seleccionado
-    let ciudad = event.target.value
-    loadDayForecastData(ciudad);
+      /*let product = xml.getElementsByTagName('product');
+      let text = document.getElementById('text').value;
     
-    let padre = document.getElementsByClassName("list-group")
-    padre = padre[0]
-    padre.innerHTML = " "
-    
-    
-    let element = document.getElementById('loadinfo');
+      for (let elemento of product){
 
-    element.addEventListener('click', (event) => {
+        if (text == xml.getElementsByTagName('name') || text==xml.getElementsByTagName('type')){
+          let src = element.getElementsByTagName('src')[0].innerHTML;
+          let name = element.getElementsByTagName('name')[0].innerHTML;
+          let type = element.getElementsByTagName('type')[0].innerHTML;
+          let price = element.getElementsByTagName('price')[0].innerHTML;
+      
+          let plantilla = `<div class="col-xl-3 col-md-6 mb-xl-0 mb-4 mt-4">
+              <div class="card card-blog card-plain">
+              <div class="card-header p-0 mt-n4 mx-3">
+                <a class="d-block shadow-xl border-radius-xl">
+                  <img src="${src}" alt="${name}" class="img-fluid shadow border-radius-xl">
+                </a>
+              </div>
+              <div class="card-body p-3">
+                <p class="mb-0 text-sm">${type}</p>
+                  <a href="javascript:;">
+                    <h5>${name}</h5>
+                  </a>
+                <p class="mb-4 text-sm">
+                  <b>Price: </b> $ ${price}
+                </p>
+                </div>
+                </div>
+              </div>`
     
-        loadWeekForecastData(ciudad);
-    });
+          text.innerHTML+=plantilla;
+        }
 
-
-});*/
+      }*/
